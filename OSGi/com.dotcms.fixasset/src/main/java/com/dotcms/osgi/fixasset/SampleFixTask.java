723936e937ef4d644e9a0d7f42cb0e62053b5d20
Jonathan Gamba <jonathan.gamba@dotcms.com>
2017-02-27 11:23:42 -0600

    Clean up

--- OSGi/com.dotcms.fixasset/src/main/java/com/dotcms/osgi/fixasset/SampleFixTask.java
+++ OSGi/com.dotcms.fixasset/src/main/java/com/dotcms/osgi/fixasset/SampleFixTask.java
@@ -1,10 +1,5 @@
 package com.dotcms.osgi.fixasset;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
 import com.dotmarketing.beans.FixAudit;
 import com.dotmarketing.common.db.DotConnect;
 import com.dotmarketing.db.HibernateUtil;
@@ -14,10 +9,12 @@ import com.dotmarketing.fixtask.FixTask;
 import com.dotmarketing.portlets.cmsmaintenance.ajax.FixAssetsProcessStatus;
 import com.dotmarketing.util.Logger;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class SampleFixTask implements FixTask {
-
-
 
     public List<Map<String, Object>> executeFix() throws DotDataException, DotRuntimeException {
 
@@ -25,16 +22,12 @@ public class SampleFixTask implements FixTask {
         int total = 0;
         List<Map<String, Object>> returnValue = new ArrayList<Map<String, Object>>();
 
-
-
         if (!FixAssetsProcessStatus.getRunning()) {
             FixAssetsProcessStatus.startProgress();
             FixAssetsProcessStatus
                     .setDescription("task " + this.getClass().getName() + ": Fixing things!");
             HibernateUtil.startTransaction();
             System.out.println("task " + this.getClass().getName() + ": Fixing things!");
-
-
 
             FixAudit Audit = new FixAudit();
             Audit.setTableName("contentlet");
@@ -55,10 +48,7 @@ public class SampleFixTask implements FixTask {
 
         }
         return returnValue;
-
     }
-
-
 
     public boolean shouldRun() {
         System.out.println("task " + this.getClass().getName() + ": shouldRun()");
@@ -66,14 +56,9 @@ public class SampleFixTask implements FixTask {
         final String countInodes = "select count(*) as test from inode";
         dc.setSQL(countInodes);
         return dc.getInt("test") > 0;
-
-
     }
 
-
-    private List<Map<String, String>> modifiedData = new ArrayList<Map<String, String>>();
-
-
+    private List<Map<String, String>> modifiedData = new ArrayList<>();
 
     @Override
     public List<Map<String, String>> getModifiedData() {
--- OSGi/com.dotcms.rest/src/main/java/com/dotcms/plugin/rest/Activator.java
+++ OSGi/com.dotcms.rest/src/main/java/com/dotcms/plugin/rest/Activator.java
@@ -9,19 +9,16 @@ public class Activator extends GenericBundleActivator {
 
 	Class clazz = ExampleResource.class;
 
-
 	public void start(BundleContext context) throws Exception {
 
 		Logger.info(this.getClass(), "Adding new Restful Service:" + clazz.getSimpleName());
 		RestServiceUtil.addResource(clazz);
-
 	}
 
 	public void stop(BundleContext context) throws Exception {
 
 		Logger.info(this.getClass(), "Removing new Restful Service:" + clazz.getSimpleName());
 		RestServiceUtil.removeResource(clazz);
-
 	}
 
 }
