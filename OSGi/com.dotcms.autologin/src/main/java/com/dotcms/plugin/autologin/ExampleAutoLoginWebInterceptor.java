120a69695e77478da954ff2dda96ebaa39decf15
Jonathan <jsanca@Jonathans-MacBook-Pro-2.local>
2017-09-13 12:12:53 -0600

    Added a variant for the autologin example with hardcodes values

--- OSGi/com.dotcms.autologin/src/main/java/com/dotcms/plugin/autologin/ExampleAutoLoginWebInterceptor.java
+++ OSGi/com.dotcms.autologin/src/main/java/com/dotcms/plugin/autologin/ExampleAutoLoginWebInterceptor.java
@@ -4,8 +4,11 @@ import com.dotcms.cms.login.LoginServiceAPI;
 import com.dotcms.filters.interceptor.Result;
 import com.dotcms.filters.interceptor.WebInterceptor;
 import com.dotmarketing.business.APILocator;
+import com.dotmarketing.business.UserAPI;
 import com.dotmarketing.cms.factories.PublicEncryptionFactory;
-import com.dotmarketing.util.UtilMethods;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotSecurityException;
+import com.liferay.portal.model.User;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,33 +20,69 @@ import java.io.IOException;
  */
 public class ExampleAutoLoginWebInterceptor implements WebInterceptor {
 
-    private final LoginServiceAPI loginServiceAPI;
+    private  final LoginServiceAPI loginServiceAPI;
+    private  final UserAPI userAPI;
 
     public ExampleAutoLoginWebInterceptor() {
-        this(APILocator.getLoginServiceAPI());
+        this(APILocator.getLoginServiceAPI(), APILocator.getUserAPI());
     }
 
-    public ExampleAutoLoginWebInterceptor(final LoginServiceAPI loginServiceAPI) {
+    public ExampleAutoLoginWebInterceptor(final LoginServiceAPI loginServiceAPI,
+                                          final UserAPI userAPI) {
         this.loginServiceAPI = loginServiceAPI;
+        this.userAPI         = userAPI;
     }
 
     @Override
     public Result intercept(final HttpServletRequest request,
                             final HttpServletResponse response) throws IOException {
         final HttpSession session  = request.getSession(false);
-        final String      userId   = request.getParameter("exampleUserId");
         Result result = Result.NEXT;
 
-        if (UtilMethods.isSet(userId) && null != session) {
+        if (null != session) {
 
-            if (this.loginServiceAPI.doCookieLogin(PublicEncryptionFactory.encryptString
-                    (userId), request, response)) {
+            final String userId = getUserId(request, session);
 
-                System.out.println("Autologin done for the user: " + userId);
-                result = Result.SKIP;
+            if (null != userId) {
+
+                final User user;
+
+                try {
+                    user = this.userAPI.loadUserById
+                            (userId, this.userAPI.getSystemUser(), false);
+
+                    if (null != user) {
+                        if (this.checkUserRoles (user)) {
+
+                            // If the user is active, so do the login
+                            if (this.loginServiceAPI.doCookieLogin // LDAP communication and checks the user exist
+                                    (PublicEncryptionFactory.encryptString
+                                            (userId), request, response)) {
+
+
+                                System.out.println("Autologin done for the user: " + userId);
+                                result = Result.SKIP;
+                            }
+                        } else {
+                            // login with the guest user, since the user does not have valid roles.
+                        }
+                    }
+                } catch (DotDataException | DotSecurityException e) {
+                    // todo: do something or log me
+                }
             }
         }
 
         return result;
     } // intercept
+
+    private boolean checkUserRoles(User user) {
+        // check the user roles
+        return true;
+    }
+
+    private String getUserId(final HttpServletRequest request, final HttpSession session) {
+        // todo: get the user from the session, cookie or whatever.
+        return "dotcms.org.1"; //(String)session.getAttribute("userEmail");
+    }
 }
