ef7d83dfc83fe7b619b3cd61ad3c74f2c864d8a9
Jonathan Gamba <jonathan.gamba@dotcms.com>
2017-10-10 13:27:42 -0600

    Upgrading skeleton example #12622

--- OSGi/com.dotcms.dynamic.skeleton/src/main/java/com/dotcms/osgi/Activator.java
+++ OSGi/com.dotcms.dynamic.skeleton/src/main/java/com/dotcms/osgi/Activator.java
@@ -1,7 +1,10 @@
 package com.dotcms.osgi;
 
-import org.osgi.framework.BundleContext;
+import com.dotcms.repackage.org.apache.logging.log4j.LogManager;
+import com.dotcms.repackage.org.apache.logging.log4j.core.LoggerContext;
+import com.dotmarketing.loggers.Log4jUtil;
 import com.dotmarketing.osgi.GenericBundleActivator;
+import org.osgi.framework.BundleContext;
 
 /**
  * This class will be used by OSGi as the entry point to your code during the bundle
@@ -9,8 +12,20 @@ import com.dotmarketing.osgi.GenericBundleActivator;
  */
 public class Activator extends GenericBundleActivator {
 
+    private LoggerContext pluginLoggerContext;
+
     @Override
     public void start ( BundleContext bundleContext ) throws Exception {
+
+        //Initializing log4j...
+        LoggerContext dotcmsLoggerContext = Log4jUtil.getLoggerContext();
+        //Initialing the log4j context of this plugin based on the dotCMS logger context
+        pluginLoggerContext = (LoggerContext) LogManager
+                .getContext(this.getClass().getClassLoader(),
+                        false,
+                        dotcmsLoggerContext,
+                        dotcmsLoggerContext.getConfigLocation());
+
         //Initializing services...
         initializeServices( bundleContext );
         
@@ -29,5 +44,11 @@ public class Activator extends GenericBundleActivator {
     public void stop ( BundleContext bundleContext ) throws Exception {
 		// remember to "unregister" anything you registered at the "start" method
 		// this will ensure the working consistency of the system.
+
+        //Unregister all the bundle services
+        unregisterServices(bundleContext);
+
+        //Shutting down log4j in order to avoid memory leaks
+        Log4jUtil.shutdown(pluginLoggerContext);
     }
 } // EOC Activator
