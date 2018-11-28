f5e9905c089565eecb30154053ff46479972684c
jdotcms <jonathan.sanchez@dotcms.com>
2018-11-28 14:44:03 -0600

    Plugin show case for interceptors

---
+++ OSGi/com.dotcms.webinterceptor/src/main/java/com/dotmarketing/osgi/webinterceptors/Activator.java
@@ -0,0 +1,77 @@
+package com.dotmarketing.osgi.webinterceptors;
+
+import com.dotcms.filters.interceptor.FilterWebInterceptorProvider;
+import com.dotcms.filters.interceptor.WebInterceptorDelegate;
+import com.dotcms.filters.interceptor.cas.CasAutoLoginWebInterceptor;
+import com.dotmarketing.filters.AutoLoginFilter;
+import com.dotmarketing.filters.CMSFilter;
+import com.dotmarketing.osgi.GenericBundleActivator;
+import com.dotmarketing.util.Config;
+import org.apache.felix.http.api.ExtHttpService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+
+import javax.servlet.Servlet;
+
+public class Activator extends GenericBundleActivator {
+
+    private Servlet simpleServlet;
+    private ExtHttpService httpService;
+    private String interceptorName;
+
+    @SuppressWarnings ("unchecked")
+    public void start ( BundleContext context ) throws Exception {
+
+        //Initializing services...
+        initializeServices( context );
+
+        //Service reference to ExtHttpService that will allows to register servlets and filters
+        ServiceReference sRef = context.getServiceReference( ExtHttpService.class.getName() );
+
+        if ( sRef != null ) {
+
+            httpService = (ExtHttpService) context.getService( sRef );
+            try {
+                //Registering a simple test servlet
+                simpleServlet = new HelloWorldServlet( );
+                httpService.registerServlet( "/helloworld", simpleServlet, null, null );
+
+                //Registering a simple test filter
+            } catch ( Exception e ) {
+                e.printStackTrace();
+            }
+
+            final FilterWebInterceptorProvider filterWebInterceptorProvider =
+                    FilterWebInterceptorProvider.getInstance(Config.CONTEXT);
+
+            final WebInterceptorDelegate delegate =
+                        filterWebInterceptorProvider.getDelegate(AutoLoginFilter.class);
+
+            final PreWebInterceptor preWebInterceptor = new PreWebInterceptor();
+            this.interceptorName = preWebInterceptor.getName();
+            delegate.addFirst(preWebInterceptor);
+        }
+
+        CMSFilter.addExclude( "/app/helloworld" );
+    }
+
+    public void stop ( BundleContext context ) throws Exception {
+
+        //Unregister the servlet
+        if ( httpService != null && simpleServlet != null ) {
+            httpService.unregisterServlet( simpleServlet );
+        }
+
+        final FilterWebInterceptorProvider filterWebInterceptorProvider =
+                FilterWebInterceptorProvider.getInstance(Config.CONTEXT);
+
+        final WebInterceptorDelegate delegate =
+                filterWebInterceptorProvider.getDelegate(AutoLoginFilter.class);
+
+        delegate.remove(this.interceptorName, true);
+
+        CMSFilter.removeExclude( "/app/helloworld" );
+    }
+
+}
---
+++ OSGi/com.dotcms.webinterceptor/src/main/java/com/dotmarketing/osgi/webinterceptors/PreWebInterceptor.java
@@ -0,0 +1,80 @@
+package com.dotmarketing.osgi.webinterceptors;
+
+import com.dotcms.filters.interceptor.Result;
+import com.dotcms.filters.interceptor.WebInterceptor;
+import com.dotmarketing.util.UtilMethods;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.IOException;
+import java.util.Map;
+
+import static com.dotcms.util.CollectionsUtils.map;
+
+/**
+ * This web interceptor adds the access control allow origin in addition to overrides the request and response
+ * @author jsanca
+ */
+public class PreWebInterceptor implements WebInterceptor {
+
+    @Override
+    public String[] getFilters() {
+        return new String[] {"/app/helloworld"};
+    }
+
+    @Override
+    public Result intercept(final HttpServletRequest request,
+                            final HttpServletResponse response) throws IOException {
+
+        Result result = Result.NEXT;
+
+        final String action = request.getParameter("action");
+        switch (action) {
+
+            case "wrap":
+                result = new Result.Builder().next()
+                    .wrap(new PreloadRequest(request))
+                    .wrap(new HeaderDecoratorResponse(response, request.getHeader("Origin"))).build();
+            break;
+            case "break":
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getOutputStream().println("Breaking the interceptor, bad request");
+                result = Result.SKIP_NO_CHAIN;
+            break;
+        }
+
+        return result;
+    }
+
+    // just includes some dummy attributes by default
+    private static class PreloadRequest extends HttpServletRequestWrapper {
+
+        private Map<String, Object> attributes = map(
+                "hello", "This is a hello test",
+                "pi", new Double(3.14159265359),
+                "goldenratio", new Double(1.6180339887));
+
+        public PreloadRequest(final HttpServletRequest request) {
+            super(request);
+        }
+
+        @Override
+        public Object getAttribute(final String name) {
+            return this.attributes.getOrDefault(name, super.getAttribute(name));
+        }
+    }
+
+    private static class HeaderDecoratorResponse extends HttpServletResponseWrapper {
+
+        public HeaderDecoratorResponse(final HttpServletResponse response, final String origin) {
+            super(response);
+            this.setHeader("Access-Control-Allow-Origin", UtilMethods.isSet(origin)?origin:"http://dotcms.webinterceptor.com");
+            this.setHeader("Access-Control-Allow-Credentials", "true");
+            this.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+            this.setHeader("Access-Control-Max-Age", "3600");
+            this.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        }
+    }
+}
