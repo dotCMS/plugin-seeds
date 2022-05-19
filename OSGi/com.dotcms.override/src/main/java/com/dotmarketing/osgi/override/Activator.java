636bfe8784191f7a85c4f8701b4ab1898a7f45c5
jdotcms <jonathan.sanchez@dotcms.com>
2022-05-19 22:56:56 +0200

    Adding a diff approach to override classes

--- OSGi/com.dotcms.override/src/main/java/com/dotmarketing/osgi/override/Activator.java
+++ OSGi/com.dotcms.override/src/main/java/com/dotmarketing/osgi/override/Activator.java
@@ -3,6 +3,13 @@ package com.dotmarketing.osgi.override;
 import com.dotmarketing.loggers.Log4jUtil;
 import com.dotmarketing.osgi.GenericBundleActivator;
 import com.dotmarketing.portlets.folders.model.Folder;
+import com.dotmarketing.util.Logger;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.pool.TypePool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.osgi.framework.BundleContext;
@@ -29,13 +36,35 @@ public class Activator extends GenericBundleActivator {
 
         //Initializing services...
         initializeServices( context );
-
+        rebase();
         /*
          * Trying to use our custom implementation of this class, after the last method call should be possible
          * if was added the Override-Classes property inside the MANIFEST.MF.
          */
         Folder folder = new Folder();
         folder.getPath();
+    }
+
+    private void rebase () throws ClassNotFoundException {
+
+        final String className = "com.dotmarketing.portlets.folders.model.Folder";
+        try {
+            ByteBuddyAgent.install();
+            final ClassLoader classLoader = getBundleClassloader();
+            final TypePool typePool = TypePool.Default.of(classLoader);
+            final TypeDescription typeDescription = typePool.describe(className).resolve();
+            final ClassFileLocator locator = ClassFileLocator.ForClassLoader.of(classLoader);
+            final ByteBuddy byteBuddy = new ByteBuddy();
+            final Class<?> loaded = byteBuddy
+                    .rebase(typeDescription, locator)
+                    .name(className)
+                    .make()
+                    .load(getWebAppClassloader(), this.getClassReloadingStrategy())
+                    .getLoaded();
+        } catch (Throwable e) {
+
+            Logger.info(this.getClass().getName(), "Error trying to rebase, probably was already rebased: " + e.getMessage());
+        }
     }
 
     public void stop ( BundleContext context ) throws Exception {
